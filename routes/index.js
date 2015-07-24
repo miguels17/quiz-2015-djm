a6e83481ffb58f5d8661b6e3ae07a0e196d03b9f
miguels17 <miguelsalas17djm@gmail.com>
2015-07-24 11:54:43 +0200

    Crear preguntas

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,8 @@ router.get('/author');
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
 router.get('/author', authorController.author);
 
 module.exports = router;
